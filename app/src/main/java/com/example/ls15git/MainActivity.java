22cf926ebe4692e8e3be15e302c57de14d66901e
ThinhVH <thinhvh.fpt@gmail.com>
2021-05-09 15:57:09 +0700

    thinh code lan 1

--- app/src/main/java/com/example/ls15git/MainActivity.java
+++ app/src/main/java/com/example/ls15git/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // thinh code lan 1
     }
 }
